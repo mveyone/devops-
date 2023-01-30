60772081168d60f26931e552b0cb9c7c2bb4f09d
mveyone <mveyone@outlook.fr>
2023-01-30 17:31:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# best-node-ejs-last-mongodb-docker
+# pvc-mongodb-app-k8s
+# devops-
---
+++ terraform/compute.tf
@@ -0,0 +1,52 @@
+resource "aws_instance" "jenkins-instance" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+  key_name      = "ubuntu-key"
+  security_groups = ["${aws_security_group.UbuntuSG.name}"]
+  user_data = file("install_jenkins.sh")
+
+  tags  = {
+    Name  = "jenkins-EC2"
+  }
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+    encrypted   = false
+  }
+}
+
+resource "aws_instance" "ansible-instance" {
+  ami           = var.ami
+  instance_type = "t2.micro"
+  key_name      = "ubuntu-key"
+  security_groups = ["${aws_security_group.UbuntuSG.name}"]
+  user_data = file("install_ansible-docker.sh")
+
+  tags  = {
+    Name  = "ansible-EC2"
+  }
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+    encrypted   = false
+  }
+}
+
+resource "aws_instance" "k8s-instance" {
+  ami           = var.ami
+  instance_type = "t2.medium"
+  key_name      = "ubuntu-key"
+  security_groups = ["${aws_security_group.UbuntuSG.name}"]
+  user_data = file("install_k8s.sh")
+
+  tags  = {
+    Name  = "k8s-EC2"
+  }
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+    encrypted   = false
+  }
+}
+
+
